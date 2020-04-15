84e588e39cbcd9141bad627d9c321c6b9b72f0e8
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-04-15 22:34:34 +0200

    Add MainActivity view and add validateLogin function in UiThread

--- app/src/main/java/com/davidmendozamartinez/coroutines/sample/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/coroutines/sample/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.davidmendozamartinez.coroutines.sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submit.setOnClickListener {
+            val success = validateLogin(username.text.toString(), password.text.toString())
+            toast(if (success) "Success" else "Failure")
+        }
     }
+
+    private fun validateLogin(username: String, password: String): Boolean {
+        Thread.sleep(2000)
+        return username.isNotEmpty() && password.isNotEmpty()
+    }
+}
+
+private fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
