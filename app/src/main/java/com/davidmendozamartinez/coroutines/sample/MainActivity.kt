6e93e9af0e8e26701a002229c7d05aaeb11b1023
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-04-16 20:05:14 +0200

    Implement CoroutineScope in MainActivity

--- app/src/main/java/com/davidmendozamartinez/coroutines/sample/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/coroutines/sample/MainActivity.kt
@@ -5,19 +5,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    private lateinit var job: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        job = SupervisorJob()
+
         submit.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
+            launch {
                 val success = withContext(Dispatchers.IO) {
                     validateLogin(
                         username.text.toString(),
@@ -32,6 +37,11 @@ class MainActivity : AppCompatActivity() {
     private fun validateLogin(username: String, password: String): Boolean {
         Thread.sleep(2000)
         return username.isNotEmpty() && password.isNotEmpty()
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
     }
 }
 
